110eec83ecf7dc3dd10fc3b5071d02998d5de7e1
HwangInseob <hwangis941126@gmail.com>
2026-03-04 17:23:45 +0900

    경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pages 배포를 위해 상대 경로로 설정
+  base: '/studiolikit/', // GitHub Pages 배포를 위해 레포지토리 이름으로 설정
 })
