fd1e133a49892c197034ed352b45081bc969e703
HwangInseob <hwangis941126@gmail.com>
2026-03-05 12:03:59 +0900

    포커스 효과 추가

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -88,10 +88,15 @@ const Projects = () => {
                         >
                             <div className="project-image-wrapper">
                                 <img src={video.thumbnailUrl} alt={video.title} className="project-img" />
-                                <div className="project-hover-overlay">
-                                    <h3 className="project-brand-logo">{video.title}</h3>
-                                    <span className="project-cat-label">{video.category}</span>
+                                <div className="project-play-icon">
+                                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8 5V19L19 12L8 5Z" fill="white" />
+                                    </svg>
                                 </div>
+                            </div>
+                            <div className="project-details">
+                                <h3 className="project-card-title">{video.title}</h3>
+                                <p className="project-card-category">{video.category}</p>
                             </div>
                         </div>
                     ))}
